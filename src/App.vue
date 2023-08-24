90eb886e5f47f7d139af9d2d8dddc0e5957a1d38
Tony Sharples <tonysharples22@me.com>
2023-08-24 11:35:37 +0100

    Remove unnecessary views

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@
     <header>
       <div>
         <nav>
-          <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
           <router-link to="/personal">Personal</router-link>
           <router-link to="/plans">Plans</router-link>
           <router-link to="/addons">Add-ons</router-link>
