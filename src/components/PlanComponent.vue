fcc6d6de561383750e30e82ce8891e4d4b043f0c
Tony Sharples <tony.sharples@accessplanit.com>
2023-09-04 11:41:38 +0100

    Add plan component to view

---
+++ src/components/PlanComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="border-solid border-2 border-indigo-600">
+            <img :src="`../src/assets/images/${icon}.svg`" :alt="`${icon}`">
+            <p>{{ plan }}</p>
+            <p>${{ cost }}/mo</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        icon: {
+            type: String,
+            required: true
+        },
+        plan: {
+            type: String,
+            required: true
+        },
+        cost: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+<style></style>
--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -2,9 +2,18 @@
     <div>
         <h1>Select your plan</h1>
         <p>You have the option of monthly or yearly billing.</p>
-        <!-- TODO: Add plan component -->
+        <plan-component icon="icon-arcade" plan="Arcade" cost="9" />
         <!-- TODO: Add plan component -->
         <!-- TODO: Add plan component -->
         <!-- TODO: Add monthly / yearly toggle component -->
     </div>
 </template>
+<script>
+import PlanComponent from '../components/PlanComponent.vue'
+
+export default {
+    components: {
+        PlanComponent
+    }
+}
+</script>
