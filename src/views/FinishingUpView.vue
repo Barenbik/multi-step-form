57b130ff18f1f0e47f59644e3f1626a3e28262bc
Barenbik <tonysharples22@me.com>
2023-09-21 10:14:04 +0100

    Style finishing up view

--- src/views/FinishingUpView.vue
+++ src/views/FinishingUpView.vue
@@ -1,9 +1,13 @@
 <template>
-    <div>
-        <h1>Finishing up</h1>
-        <p>Double check everything looks OK before confirming.</p>
-        <!-- TODO: Add charge breakdown component -->
-        <p>Total (per month)</p>
-        <p>+$12/mo</p>
+  <div class="p-6 rounded-md bg-white shadow-lg">
+    <h1 class="font-semibold text-marine-blue text-2xl pb-2">Finishing up</h1>
+    <p class="font-normal text-cool-gray pb-6 text-lg leading-6">
+      Double check everything looks OK before confirming.
+    </p>
+    <!-- TODO: Add charge breakdown component -->
+    <div class="flex justify-between">
+      <span class="font-normal text-cool-gray text-sm">Total (per month)</span>
+      <span class="text-purplish-blue font-bold">+$12/mo</span>
     </div>
+  </div>
 </template>
