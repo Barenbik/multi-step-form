e9ab2fda4b191e87f4e597d9870d6b88238f9bcf
Barenbik <tonysharples22@me.com>
2023-09-21 09:57:45 +0100

    Style add-on view

--- src/views/AddOnView.vue
+++ src/views/AddOnView.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <h1>Pick add-ons</h1>
-        <p>Add-ons help enhance your gaming experience.</p>
+    <div class="p-6 rounded-md bg-white">
+        <h1 class="font-semibold text-marine-blue text-2xl pb-2">Pick add-ons</h1>
+        <p class="font-normal text-cool-gray pb-6 text-lg leading-6">Add-ons help enhance your gaming experience.</p>
         <!-- TODO: Add add-on component -->
         <!-- TODO: Add add-on component -->
         <!-- TODO: Add add-on component -->
