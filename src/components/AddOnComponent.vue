ed178b7fc1b42fd645943e824ca1a7d0e76a9e2e
Barenbik <tonysharples22@me.com>
2023-09-22 10:25:21 +0100

    Style add-on component

--- src/components/AddOnComponent.vue
+++ src/components/AddOnComponent.vue
@@ -3,14 +3,18 @@
     <div
       class="border-solid border border-gray-200 rounded-md flex hover:border-indigo-600 hover:bg-alabaster"
     >
-    <input type="checkbox" name="addon" id="addon">
-    <div class="p-4 pl-0">
-        <p class="font-semibold text-marine-blue">{{ addon }}</p>
-        <p class="font-normal text-cool-gray text-sm">{{ description }}</p>
-        <p v-if="!isYearly" class="font-normal text-sm text-purplish-blue">${{ monthlyCost }}/mo</p>
-        <p v-else class="font-normal text-sm text-purplish-blue">${{ yearlyCost }}/yr</p>
+      <div class="p-3 flex items-center w-full justify-stretch">
+        <input class="h-5 w-5 text-purplish-blue rounded focus:outline-none focus:ring-transparent" type="checkbox" name="addon" id="addon" />
+        <div class="pl-4 flex-grow">
+          <p class="font-semibold text-marine-blue">{{ addon }}</p>
+          <p class="font-normal text-cool-gray text-sm">{{ description }}</p>
+        </div>
+        <div class="flex justify-end">
+          <p v-if="!isYearly" class="font-normal text-sm text-purplish-blue">+${{ monthlyCost }}/mo</p>
+          <p v-else class="font-normal text-sm text-purplish-blue">+${{ yearlyCost }}/yr</p>
+        </div>        
       </div>
-</div>
+    </div>
   </div>
 </template>
 <script>
@@ -18,24 +22,24 @@ import { mapState } from 'pinia'
 import { useStore } from '@/stores/store.js'
 
 export default {
-    props: {
-        addon: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String,
-            required: true
-        },
-        monthlyCost: {
-            type: String,
-            required: true
-        },
-        yearlyCost: {
-            type: String,
-            required: true
-        }
+  props: {
+    addon: {
+      type: String,
+      required: true
     },
+    description: {
+      type: String,
+      required: true
+    },
+    monthlyCost: {
+      type: String,
+      required: true
+    },
+    yearlyCost: {
+      type: String,
+      required: true
+    }
+  },
   computed: {
     ...mapState(useStore, ['isYearly'])
   }
