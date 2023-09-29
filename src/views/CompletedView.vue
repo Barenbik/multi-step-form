98ed7dab7d225684b92581cf90863cc6439cfba7
Barenbik <tonysharples22@me.com>
2023-09-29 10:38:40 +0100

    Style completed view

--- src/views/CompletedView.vue
+++ src/views/CompletedView.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    <img src="../assets/images/icon-thank-you.svg" alt="Thank you" />
-    <h1>Thank you!</h1>
-    <p>
-      Thanks for confirming your subscription! We hope you have fun using our platform. If you ever
-      need support, please feel free to email us at support@loremgaming.com
-    </p>
+  <div class="mx-4 -my-20 p-6 rounded-md bg-white shadow-lg">
+    <div class="flex flex-col items-center py-10">
+      <img src="../assets/images/icon-thank-you.svg" alt="Thank you" class="py-4 img-max" />
+      <h1 class="font-semibold text-marine-blue text-2xl py-2">Thank you!</h1>
+      <p class="text-center text-cool-gray">Thanks for confirming your subscription!</p>
+      <p class="text-center text-cool-gray">
+        We hope you have fun using our platform. If you ever need support, please feel free to email
+        us at support@loremgaming.com
+      </p>
+    </div>
   </div>
 </template>
