a7496298959090bc7d7756b1f0b4ed5a879048e8
Barenbik <tonysharples22@me.com>
2023-10-03 10:48:31 +0100

    Add labels to stages

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,36 @@
             >1</router-link
           >
         </div>
+        <div class="hidden xl:block">
+          <div class="text-light-gray text-sm">STEP 1</div>
+          <div class="text-white">YOUR INFO</div>
+        </div>
         <div class="m-2 rounded-full border border-solid border-white hover:bg-light-blue">
           <router-link to="/plans" class="text-white p-2 font-medium hover:text-marine-blue"
             >2</router-link
           >
+        </div>
+        <div class="hidden xl:block">
+          <div class="text-light-gray text-sm">STEP 2</div>
+          <div class="text-white">SELECT PLAN</div>
         </div>
         <div class="m-2 rounded-full border border-solid border-white hover:bg-light-blue">
           <router-link to="/addons" class="text-white p-2 font-medium hover:text-marine-blue"
             >3</router-link
           >
         </div>
+        <div class="hidden xl:block">
+          <div class="text-light-gray text-sm">STEP 3</div>
+          <div class="text-white">ADD-ONS</div>
+        </div>
         <div class="m-2 rounded-full border border-solid border-white hover:bg-light-blue">
           <router-link to="/finishingup" class="text-white p-2 font-medium hover:text-marine-blue"
             >4</router-link
           >
+        </div>
+        <div class="hidden xl:block">
+          <div class="text-light-gray text-sm">STEP 4</div>
+          <div class="text-white">SUMMARY</div>
         </div>
         <div>
           <router-link to="/completed"></router-link>
