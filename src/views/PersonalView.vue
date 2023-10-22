42e1f7545f2826043b309fe73828cb9af6feee3b
Tony Sharples <tonysharples22@me.com>
2023-10-22 14:07:38 +0100

    Format syntax on views

--- src/views/PersonalView.vue
+++ src/views/PersonalView.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="mx-4 -my-20 p-6 rounded-md bg-white shadow-lg md:my-4 md:p-4 md:shadow-none md:flex md:flex-row">
+  <div
+    class="mx-4 -my-20 p-6 rounded-md bg-white shadow-lg md:my-4 md:p-4 md:shadow-none md:flex md:flex-row"
+  >
     <stage-component class="rounded-md hidden md:block" />
     <div class="md:px-20 md:pt-10">
       <h1 class="font-semibold text-marine-blue text-3xl pb-2">Personal info</h1>
@@ -8,18 +10,33 @@
       </p>
       <div class="pb-5">
         <label class="block text-marine-blue text-sm pb-1" for="name">Name</label>
-        <input class="border border-gray-300 py-2 px-4 rounded-md w-full" type="text" name="name" id="name"
-          placeholder="e.g. Stephen King" />
+        <input
+          class="border border-gray-300 py-2 px-4 rounded-md w-full"
+          type="text"
+          name="name"
+          id="name"
+          placeholder="e.g. Stephen King"
+        />
       </div>
       <div class="pb-5">
         <label class="block text-marine-blue text-sm pb-1" for="email">Email Address</label>
-        <input class="border border-gray-300 py-2 px-4 rounded-md w-full" type="email" name="email" id="email"
-          placeholder="e.g. stephenking@lorem.com" />
+        <input
+          class="border border-gray-300 py-2 px-4 rounded-md w-full"
+          type="email"
+          name="email"
+          id="email"
+          placeholder="e.g. stephenking@lorem.com"
+        />
       </div>
       <div class="pb-5">
         <label class="block text-marine-blue text-sm pb-1" for="phone">Phone Number</label>
-        <input class="border border-gray-300 py-2 px-4 rounded-md w-full" type="tel" name="phone" id="phone"
-          placeholder="e.g. +1 234 567 890" />
+        <input
+          class="border border-gray-300 py-2 px-4 rounded-md w-full"
+          type="tel"
+          name="phone"
+          id="phone"
+          placeholder="e.g. +1 234 567 890"
+        />
       </div>
       <navigation-component moveTo="/plans" class="hidden md:block" />
     </div>
@@ -27,8 +44,8 @@
   <navigation-component moveTo="/plans" class="md:hidden" />
 </template>
 <script>
-import NavigationComponent from '../components/NavigationComponent.vue';
-import StageComponent from "../components/StageComponent.vue";
+import NavigationComponent from '../components/NavigationComponent.vue'
+import StageComponent from '../components/StageComponent.vue'
 
 export default {
   components: {
--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="mx-4 -my-20 p-6 rounded-md bg-white shadow-lg md:my-4 md:p-4 md:shadow-none md:flex md:flex-row">
-      <stage-component class="rounded-md hidden md:block" />
-      <div class="md:px-20 md:pt-10">
-        <h1 class="font-semibold text-marine-blue text-3xl pb-2">Select your plan</h1>
-        <p class="font-normal text-cool-gray pb-8 leading-6">
-          You have the option of monthly or yearly billing.
-        </p>
-        <div class="md:flex md:flex-row">
-          <plan-component icon="icon-arcade" plan="Arcade" monthlyCost="9" yearlyCost="90" />
-          <plan-component icon="icon-advanced" plan="Advanced" monthlyCost="12" yearlyCost="120" />
-          <plan-component icon="icon-pro" plan="Pro" monthlyCost="15" yearlyCost="150" />
-        </div>
-        <toggle-switch />
-        <navigation-component backTo="/personal" moveTo="/addons" class="hidden md:block" />
+  <div
+    class="mx-4 -my-20 p-6 rounded-md bg-white shadow-lg md:my-4 md:p-4 md:shadow-none md:flex md:flex-row"
+  >
+    <stage-component class="rounded-md hidden md:block" />
+    <div class="md:px-20 md:pt-10">
+      <h1 class="font-semibold text-marine-blue text-3xl pb-2">Select your plan</h1>
+      <p class="font-normal text-cool-gray pb-8 leading-6">
+        You have the option of monthly or yearly billing.
+      </p>
+      <div class="md:flex md:flex-row">
+        <plan-component icon="icon-arcade" plan="Arcade" monthlyCost="9" yearlyCost="90" />
+        <plan-component icon="icon-advanced" plan="Advanced" monthlyCost="12" yearlyCost="120" />
+        <plan-component icon="icon-pro" plan="Pro" monthlyCost="15" yearlyCost="150" />
       </div>
+      <toggle-switch />
+      <navigation-component backTo="/personal" moveTo="/addons" class="hidden md:block" />
     </div>
+  </div>
   <navigation-component backTo="/personal" moveTo="/addons" class="md:hidden" />
 </template>
 <script>
