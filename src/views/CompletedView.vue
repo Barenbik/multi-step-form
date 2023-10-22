bda1c3060dca8cb8f772cb90e56db948042a0ffc
Tony Sharples <tonysharples22@me.com>
2023-10-22 14:07:20 +0100

    Style completed view

--- src/views/CompletedView.vue
+++ src/views/CompletedView.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="mx-4 -my-20 p-6 rounded-md bg-white shadow-lg">
-    <div class="flex flex-col items-center py-10">
-      <img src="../assets/images/icon-thank-you.svg" alt="Thank you" class="py-4 img-max" />
-      <h1 class="font-semibold text-marine-blue text-2xl py-2">Thank you!</h1>
-      <p class="text-center text-cool-gray">Thanks for confirming your subscription!</p>
-      <p class="text-center text-cool-gray">
-        We hope you have fun using our platform. If you ever need support, please feel free to email
-        us at support@loremgaming.com
-      </p>
+  <div
+    class="mx-4 -my-20 p-6 rounded-md bg-white shadow-lg md:my-4 md:shadow-none md:flex md:flex-row"
+  >
+    <stage-component class="rounded-md hidden md:block" />
+    <div class="md:px-20 md:py-20">
+      <div class="flex flex-col items-center py-10 px-8">
+        <img src="../assets/images/icon-thank-you.svg" alt="Thank you" class="py-4 img-max" />
+        <h1 class="font-semibold text-marine-blue text-3xl py-2 md:py-4">Thank you!</h1>
+        <p class="text-center text-cool-gray md:pb-10">
+          Thanks for confirming your subscription! We hope you have<br class="hidden md:block" />
+          fun using our platform. If you ever need support, please feel<br
+            class="hidden md:block"
+          />
+          free to email us at support@loremgaming.com
+        </p>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+import StageComponent from '../components/StageComponent.vue'
+
+export default {
+  components: {
+    StageComponent
+  }
+}
+</script>
