43bd0462c345894798d1d3e19380834cb652e807
Barenbik <tonysharples22@me.com>
2023-10-30 16:47:50 +0000

    Align personal view elements

--- src/views/PersonalView.vue
+++ src/views/PersonalView.vue
@@ -3,7 +3,7 @@
     class="mx-4 -my-20 p-6 rounded-md bg-white shadow-lg md:my-4 md:p-4 md:shadow-none md:flex md:flex-row md:h-3/4 md:min-h-custom"
   >
     <stage-component class="rounded-md hidden md:block" />
-    <div class="flex flex-col md:px-20 md:pt-10 md:h-full">
+    <div class="flex flex-col md:px-20 md:pt-10 md:h-full md:w-full">
       <div>
         <h1 class="font-semibold text-marine-blue text-3xl pb-2">Personal info</h1>
         <p class="font-normal text-cool-gray pb-8 leading-6">
