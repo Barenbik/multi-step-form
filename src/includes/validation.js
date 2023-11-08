acc48a94d4cb9789d4b6812b0fa0175eb8ca7a38
Barenbik <tonysharples22@me.com>
2023-11-08 11:53:53 +0000

    Add validation component to personal view

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,13 +1,15 @@
-import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate'
-import { required, email } from '@vee-validate/rules'
+import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage as VeeError } from 'vee-validate'
+import { required, email, alpha_spaces, integer } from '@vee-validate/rules'
 
 export default {
   install(app) {
     app.component('VeeForm', VeeForm)
     app.component('VeeField', VeeField)
-    app.component('ErrorMessage', ErrorMessage)
+    app.component('ErrorMessage', VeeError)
 
     defineRule('required', required)
     defineRule('email', email)
+    defineRule('alpha_spaces', alpha_spaces)
+    defineRule('integer', integer)
   }
 }
--- src/views/PersonalView.vue
+++ src/views/PersonalView.vue
@@ -1,58 +1,68 @@
 <template>
-  <div
-    class="mx-4 -my-20 p-6 rounded-md bg-white shadow-lg md:my-4 md:p-4 md:shadow-none md:flex md:flex-row md:h-custom md:min-h-custom"
-  >
-    <stage-component class="rounded-md hidden md:block" />
-    <div class="flex flex-col md:px-20 md:pt-10 md:h-full md:w-full">
-      <div>
-        <h1 class="font-semibold text-marine-blue text-3xl pb-2">Personal info</h1>
-        <p class="font-normal text-cool-gray pb-8 leading-6">
-          Please provide your name, email address, and phone number.
-        </p>
-        <div class="pb-5">
-          <label class="block text-marine-blue text-sm pb-1" for="name">Name</label>
-          <input
-            class="border border-gray-300 py-2 px-4 rounded-md w-full"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="e.g. Stephen King"
-          />
+  <vee-form :validation-schema="schema" @submit.prevent="onSubmit">
+    <div
+      class="mx-4 -my-20 p-6 rounded-md bg-white shadow-lg md:my-4 md:p-4 md:shadow-none md:flex md:flex-row md:h-custom md:min-h-custom"
+    >
+      <stage-component class="rounded-md hidden md:block" />
+      <div class="flex flex-col md:px-20 md:pt-10 md:h-full md:w-full">
+        <div>
+          <h1 class="font-semibold text-marine-blue text-3xl pb-2">Personal info</h1>
+          <p class="font-normal text-cool-gray pb-8 leading-6">
+            Please provide your name, email address, and phone number.
+          </p>
+          <div class="pb-5">           
+            <label class="block text-marine-blue text-sm pb-1" for="name">Name</label>
+            <vee-field class="border border-gray-300 py-2 px-4 rounded-md w-full"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="e.g. Stephen King" />            
+          </div>
+          <div class="pb-5">           
+            <label class="block text-marine-blue text-sm pb-1" for="email">Email Address</label>
+            <vee-field class="border border-gray-300 py-2 px-4 rounded-md w-full"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="e.g. stephenking@lorem.com" />
+          </div>
+          <div class="pb-5">
+            <label class="block text-marine-blue text-sm pb-1" for="phone">Phone Number</label>
+            <vee-field class="border border-gray-300 py-2 px-4 rounded-md w-full"
+              type="tel"
+              name="phone"
+              id="phone"
+              placeholder="e.g. +1 234 567 890" />            
+          </div>
         </div>
-        <div class="pb-5">
-          <label class="block text-marine-blue text-sm pb-1" for="email">Email Address</label>
-          <input
-            class="border border-gray-300 py-2 px-4 rounded-md w-full"
-            type="email"
-            name="email"
-            id="email"
-            placeholder="e.g. stephenking@lorem.com"
-          />
-        </div>
-        <div class="pb-5">
-          <label class="block text-marine-blue text-sm pb-1" for="phone">Phone Number</label>
-          <input
-            class="border border-gray-300 py-2 px-4 rounded-md w-full"
-            type="tel"
-            name="phone"
-            id="phone"
-            placeholder="e.g. +1 234 567 890"
-          />
-        </div>
+        <navigation-component moveTo="/plans" class="hidden md:block" />
       </div>
-      <navigation-component moveTo="/plans" class="hidden md:block" />
     </div>
-  </div>
-  <navigation-component moveTo="/plans" class="md:hidden" />
+    <navigation-component moveTo="/plans" class="md:hidden" />    
+  </vee-form>
 </template>
 <script>
 import NavigationComponent from '../components/NavigationComponent.vue'
 import StageComponent from '../components/StageComponent.vue'
 
 export default {
+  data() {
+    return {
+      schema: {
+        name: "required|alpha_spaces",
+        email: "required|email",
+        phone: "required|integer",
+      },
+    };
+  },
   components: {
     NavigationComponent,
     StageComponent
-  }
+  },
+  methods: {
+    onSubmit() {
+      console.log('Submitted');
+    },
+  },
 }
 </script>
