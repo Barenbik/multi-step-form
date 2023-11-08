90bbc55e7d52e41eacfab40d99ab14d731ce319e
Barenbik <tonysharples22@me.com>
2023-11-08 15:49:52 +0000

    Highlight current stage

--- src/components/StageComponent.vue
+++ src/components/StageComponent.vue
@@ -4,10 +4,8 @@
       class="flex items-center justify-center h-20 md:h-auto md:justify-normal md:items-start md:flex-col md:py-6 md:pr-24 md:pl-6"
     >
       <div class="md:flex md:flex-row md:py-2 md:items-center">
-        <div class="flex items-center justify-center m-3 h-8 w-8 rounded-full border border-solid border-white hover:bg-light-blue">
-          <router-link to="/personal" class="text-white font-medium hover:text-marine-blue"
-            >1</router-link
-          >
+        <div class="flex items-center justify-center m-3 h-8 w-8 rounded-full border border-solid border-white" :class="this.$route.name === 'personal' ? 'bg-light-blue border-none' : ''">
+          <router-link to="/personal" class="font-medium" :class="this.$route.name === 'personal' ? 'text-marine-blue' : 'text-white'">1</router-link>
         </div>
         <div class="hidden md:block">
           <div class="text-light-gray text-xs min-w-98">STEP 1</div>
@@ -15,8 +13,8 @@
         </div>
       </div>
       <div class="md:flex md:flex-row md:py-2 md:items-center">
-        <div class="flex items-center justify-center m-3 h-8 w-8 rounded-full border border-solid border-white hover:bg-light-blue">
-          <router-link to="/plans" class="text-white font-medium hover:text-marine-blue"
+        <div class="flex items-center justify-center m-3 h-8 w-8 rounded-full border border-solid border-white " :class="this.$route.name === 'plans' ? 'bg-light-blue border-none' : ''">
+          <router-link to="/plans" class="font-medium" :class="this.$route.name === 'plans' ? 'text-marine-blue' : 'text-white'"
             >2</router-link
           >
         </div>
@@ -26,8 +24,8 @@
         </div>
       </div>
       <div class="md:flex md:flex-row md:py-2 md:items-center">
-        <div class="flex items-center justify-center m-3 h-8 w-8 rounded-full border border-solid border-white hover:bg-light-blue">
-          <router-link to="/addons" class="text-white font-medium hover:text-marine-blue"
+        <div class="flex items-center justify-center m-3 h-8 w-8 rounded-full border border-solid border-white" :class="this.$route.name === 'addons' ? 'bg-light-blue border-none' : ''">
+          <router-link to="/addons" class="font-medium" :class="this.$route.name === 'addons' ? 'text-marine-blue' : 'text-white'"
             >3</router-link
           >
         </div>
@@ -37,8 +35,8 @@
         </div>
       </div>
       <div class="md:flex md:flex-row md:py-2 md:items-center">
-        <div class="flex items-center justify-center m-3 h-8 w-8 rounded-full border border-solid border-white hover:bg-light-blue">
-          <router-link to="/finishingup" class="text-white font-medium hover:text-marine-blue"
+        <div class="flex items-center justify-center m-3 h-8 w-8 rounded-full border border-solid border-white" :class="this.$route.name === 'finishingup' ? 'bg-light-blue border-none' : ''">
+          <router-link to="/finishingup" class="font-medium" :class="this.$route.name === 'finishingup' ? 'text-marine-blue' : 'text-white'"
             >4</router-link
           >
         </div>
